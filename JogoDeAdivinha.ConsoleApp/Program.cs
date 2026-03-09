dbc52d0c6cf4fb79eeaa34905e66a2e9c5bf5473
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-09 15:09:07 -0300

    ADD projeto adivinha

---
+++ JogoDeAdivinha.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("------------------------------");
+        Console.WriteLine("Jogo Do Adivinha !");
+        Console.WriteLine("------------------------------");
+
+        Console.ReadLine();
+    }
+}
