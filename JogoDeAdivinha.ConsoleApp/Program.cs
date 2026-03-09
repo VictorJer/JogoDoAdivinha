de2c9b3445781968a812ebd1340f972ace6f2c90
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-09 16:01:33 -0300

    ADD filtro para numeros do jogador

--- JogoDeAdivinha.ConsoleApp/Program.cs
+++ JogoDeAdivinha.ConsoleApp/Program.cs
@@ -1,29 +1,60 @@
-﻿        using System;
-        using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.Clear();
+        bool verificador = true;
 
-        Console.WriteLine("------------------------------");
-        Console.WriteLine("Jogo Do Adivinha !");
-        Console.WriteLine("------------------------------");
+        while (verificador == true)
+        {
+            Console.Clear();
 
-        Console.ReadLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Jogo Do Adivinha !");
+            Console.WriteLine("------------------------------");
 
-        int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+            Console.ReadLine();
+
+            int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
 
 
 
-        System.Console.Write("digirte um numero: ");
-        string? numeroJogador = Console.ReadLine();
-        System.Console.WriteLine("----------------------------");
+            System.Console.Write("digirte um numero: ");
+            string? numeroJogador = Console.ReadLine();
+            System.Console.WriteLine("----------------------------");
 
-        int numeroJogadorInti = Convert.ToInt32(numeroJogador);
+            int numeroJogadorInti = Convert.ToInt32(numeroJogador);
 
-        System.Console.WriteLine("Numero Digitado: " + numeroJogadorInti);
-        System.Console.WriteLine("O numero aleatorio era: " + numeroAleatorio);
+            if (numeroJogadorInti == numeroAleatorio)
+            {
+                System.Console.WriteLine("-----------------------");
+                System.Console.WriteLine("parabéns, você acertou");
+                System.Console.WriteLine("-----------------------");
+            }
+            else if (numeroJogadorInti > numeroAleatorio)
+            {
+                System.Console.WriteLine("-----------------------");
+                System.Console.WriteLine("o numero digitado era maior");
+                System.Console.WriteLine("-----------------------");
+            }
+            else
+            {
+                System.Console.WriteLine("-----------------------");
+                System.Console.WriteLine("o numero digitado e menor");
+                System.Console.WriteLine("-----------------------");
+            }
+
+            System.Console.WriteLine("Quer continuar (S/N): ");
+            string? SoN = Console.ReadLine();
+
+            if (SoN.ToUpper() != "S")
+            {
+                break;
+            }
+        }
+
+
     }
 
 }
