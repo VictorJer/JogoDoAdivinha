bede8474fa37d1029d2d6bac1da394746074ab4f
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-03-09 15:30:24 -0300

    ADD geração de numero aleatorio

--- JogoDeAdivinha.ConsoleApp/Program.cs
+++ JogoDeAdivinha.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿        using System;
+        using System.Security.Cryptography;
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -9,5 +11,19 @@
         Console.WriteLine("------------------------------");
 
         Console.ReadLine();
+
+        int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+
+
+
+        System.Console.Write("digirte um numero: ");
+        string? numeroJogador = Console.ReadLine();
+        System.Console.WriteLine("----------------------------");
+
+        int numeroJogadorInti = Convert.ToInt32(numeroJogador);
+
+        System.Console.WriteLine("Numero Digitado: " + numeroJogadorInti);
+        System.Console.WriteLine("O numero aleatorio era: " + numeroAleatorio);
     }
+
 }
